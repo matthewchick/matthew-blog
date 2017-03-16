53e7a3fa27f148ed3b4459f6dffb0c1419ab3d22
matthew chick <chikmatthew@gmail.com>
2017-03-16 22:59:05 +1100

    Add model and create db and table

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,60 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only:[:edit, :update, :show, :destroy]  #only edit, update, show and destroy methods
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    #render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    #@article.save
+    #redirect_to articles_show(@article)
+    #articles_path is for index action in controller vs article_path is for show action.
+    #Plural form is for getting many articles vs singular form is for getting a single article.
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'   #if not pass validation
+    end
+  end
+
+  def show
+    # @article = Article.find(params[:id])
+  end
+
+  def edit
+    # @article = Article.find(params[:id])
+  end
+
+  def update
+    #@article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'   #if not pass validation
+    end
+  end
+
+  def destroy
+    #@article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+
+  # http://blog.trackets.com/2013/08/17/strong-parameters-by-example.html
+  private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+end
---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+# file name: article.rb -> singular and all lowercase
+# :: is to mention the module name. ActiveRecord::Base means base class in activerecord module or namespace.
+# model name: Article -> capitalized U and singular
+class Article < ActiveRecord::Base
+   #add validation before saving to dB
+   validates :title, presence: true, length:{minimum: 3, maximum: 50 }
+   validates :description, presence: true, length:{minimum: 10, maximum: 300 }
+end

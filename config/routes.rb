316e063892567edeffb4927d68cfd1c28131ec5b
matthew chick <chikmatthew@gmail.com>
2017-03-16 22:08:03 +1100

    A userful message to help remember details of commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get 'pages/home', to: 'pages#home'         #localhost:3000/pages/home
+  #get 'pages/about', to: 'pages#about'       #localhsot:3000/pages/about
+  #localhost:3000 or localhost:3000/about
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+
+end

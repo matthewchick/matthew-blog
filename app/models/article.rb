180c192a9264faefc346b5cad38d39defa09e7b9
matthew chick <chikmatthew@gmail.com>
2017-03-24 22:00:20 +1100

    Add 1:M Association and add debugger

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,9 @@
 # :: is to mention the module name. ActiveRecord::Base means base class in activerecord module or namespace.
 # model name: Article -> capitalized U and singular
 class Article < ActiveRecord::Base
+   belongs_to :user
    #add validation before saving to dB
    validates :title, presence: true, length:{minimum: 3, maximum: 50 }
    validates :description, presence: true, length:{minimum: 10, maximum: 300 }
+   validates :user_id, presence: true
 end

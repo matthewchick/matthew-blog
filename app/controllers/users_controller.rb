13a72169a8f5404424ca5e4bb0c553b6c1392802
matthew chick <chikmatthew@gmail.com>
2017-03-30 20:53:33 +1100

    Create a login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    #for pagination
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
 
   def edit
